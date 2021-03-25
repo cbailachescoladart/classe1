7e25e446ada18c704d23e67cb53f08ce363c1e57
cbailachescoladart <81374779+cbailachescoladart@users.noreply.github.com>
2021-03-25 17:46:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Document exemple</title>
+	</head>
+
+	<body>
+		
+
+		<?php the_title(12); ?>
+
+
+		<h1>The title</h1>
+
+		<h2><?php echo "Hello world!"; ?></h2>
+		
+		<p>The content of the document...</p>
+		<img width="400" src="https://miro.medium.com/max/1838/1*MI686k5sDQrISBM6L8pf5A.jpeg">
+
+
+	</body>
+
+</html>
